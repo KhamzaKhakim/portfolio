8b812da9890917e35c27f55b99a2aaf49c422403
Khamza Khakim <hamzir.04@gmail.com>
2025-11-14 01:35:23 +0500

    Initial commit

---
+++ src/app/components/AboutMe/index.tsx
@@ -0,0 +1,3 @@
+export default function AboutMe() {
+  return <div className="h-96 bg-blue-600"></div>;
+}
---
+++ src/app/components/Hero/index.tsx
@@ -0,0 +1,19 @@
+export default function Hero() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div>
+        <h1 className="text-7xl m-6 text-center font-bold">
+          Hi, I&apos;m Khakim Khamza
+        </h1>
+        <h3 className="text-center text-2xl font-500 text-gray-500 m-6">
+          Full Stack Developer crafting exceptional digital experiences
+        </h3>
+        <h6 className="text-center text-xl text-gray-500 m-6">
+          I build modern web applications with a focus on clean code, user
+          experience, and performance.
+        </h6>
+        <div className="flex"></div>
+      </div>
+    </div>
+  );
+}
