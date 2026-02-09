967ea63b2b536498e06ec5a6336d11e59376c099
Khamza Khakim <hamzir.04@gmail.com>
2026-02-09 13:20:36 +0500

    Fix links

--- src/components/AboutMe/index.tsx
+++ src/components/AboutMe/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import Github from "@icons/github.svg";
 import Gmail from "@icons/gmail.svg";
+import Leetcode from "@icons/leetcode.svg";
 import Linkedin from "@icons/linkedin.svg";
 import Telegram from "@icons/telegram.svg";
 
@@ -30,44 +31,63 @@ export default function AboutMe() {
         systems, and turning ideas into products that feel solid and
         intentional.
       </p>
-      <div className="flex items-center gap-x-4">
-        <p>Contact Me: </p>
-        <div className="flex gap-x-2">
-          <Button
-            size="icon"
-            className="transform transition duration-300 hover:scale-110"
-          >
-            <Link
-              href={"/blog"}
+      <div className="flex items-center justify-around flex-wrap gap-4 mt-8">
+        <div className="flex items-center gap-x-4">
+          <p>Let`s connect: </p>
+          <div className="flex gap-x-2">
+            <Button
+              size="icon"
               className="transform transition duration-300 hover:scale-110"
             >
-              <Github className="size-6" fill="var(--primary)" />
-            </Link>
-          </Button>
-          <Button
-            size="icon"
-            className="transform transition duration-300 hover:scale-110"
-          >
-            <Link href={"/blog"}>
-              <Linkedin className="size-6" fill="var(--primary)" />
-            </Link>
-          </Button>
-          <Button
-            size="icon"
-            className="transform transition duration-300 hover:scale-110"
-          >
-            <Link href={"/blog"}>
-              <Telegram className="size-6" fill="var(--primary)" />
-            </Link>
-          </Button>
-          <Button
-            size="icon"
-            className="transform transition duration-300 hover:scale-110"
-          >
-            <Link href={"/blog"}>
-              <Gmail className="size-6" fill="var(--primary)" />
-            </Link>
-          </Button>
+              <Link
+                href="https://www.linkedin.com/in/khamzakhakim/"
+                target="_blank"
+              >
+                <Linkedin className="size-6" fill="var(--primary)" />
+              </Link>
+            </Button>
+            <Button
+              size="icon"
+              className="transform transition duration-300 hover:scale-110"
+            >
+              <Link href="https://t.me/baatormoz" target="_blank">
+                <Telegram className="size-6" fill="var(--primary)" />
+              </Link>
+            </Button>
+            <Button
+              size="icon"
+              className="transform transition duration-300 hover:scale-110"
+            >
+              <Link href="mailto:khakimkhamza.dev@gmail.com">
+                <Gmail className="size-6" fill="var(--primary)" />
+              </Link>
+            </Button>
+          </div>
+        </div>
+        <div className="flex items-center gap-x-4">
+          <p>Developer profiles: </p>
+          <div className="flex gap-x-2">
+            <Button
+              size="icon"
+              className="transform transition duration-300 hover:scale-110"
+            >
+              <Link
+                href="https://github.com/KhamzaKhakim"
+                target="_blank"
+                className="transform transition duration-300 hover:scale-110"
+              >
+                <Github className="size-6" fill="var(--primary)" />
+              </Link>
+            </Button>
+            <Button
+              size="icon"
+              className="transform transition duration-300 hover:scale-110"
+            >
+              <Link href="https://leetcode.com/u/baatormoz/" target="_blank">
+                <Leetcode className="size-6" fill="var(--primary)" />
+              </Link>
+            </Button>
+          </div>
         </div>
       </div>
     </div>
