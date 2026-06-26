adc731c1d25cb86ce35ca36dc25a5f5399cf4e5d
Khamza Khakim <hamzir.04@gmail.com>
2026-06-26 15:41:20 +0500

    Change document
    Old cv document get outdated

--- src/components/Header/page.tsx
+++ src/components/Header/page.tsx
@@ -4,22 +4,12 @@ import Link from "next/link";
 
 import { ThemeButton } from "../theme-button";
 import { Button } from "../ui/button";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "../ui/dialog";
+import { Dialog, DialogContent, DialogTrigger } from "../ui/dialog";
 import { Separator } from "../ui/separator";
 
 export function Header() {
   return (
-    <div
-      className="header sticky top-0 z-10 backdrop-blur-xl"
-      // style={{ viewTransitionName: "header" }}
-    >
+    <div className="header sticky top-0 z-10 backdrop-blur-xl">
       <div className="flex justify-center">
         <div className="h-16 mx-8 sm:mx-14 my-2 flex items-center w-200 justify-between">
           <Link href="/">
@@ -58,7 +48,7 @@ export function Header() {
               <DialogContent className="sm:max-w-3xl w-[95vw] h-[95vh] flex flex-col p-4">
                 <div className="flex-1 overflow-hidden rounded-md border">
                   <iframe
-                    src="Khamza_CV_2026.pdf#toolbar=1&navpanes=0&scrollbar=0&zoom=page-fit&view=Fit"
+                    src="Khamza_CV.pdf#toolbar=1&navpanes=0&scrollbar=0&zoom=page-fit&view=Fit"
                     className="w-full h-full"
                     title="CV"
                   />
